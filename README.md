ac0c66657555c9bc4b44907cc528c3f7b298733c
Hari Govind <hgs@disroot.org>
2018-08-20 15:32:49 +0530

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # flood-rescue
 A mobile friendly frontend for acessing Kerala Flood Relief  Rescue Ops data.
+
+Data is obtained from [Rescue Ops Google Sheet](https://docs.google.com/spreadsheets/d/1DtMZWjONp6Nh3hUmGXpq3r83OkTV_kKokYdDnTu6vWc/edit#gid=0) and presented in a responsive design.
+
+# Contribute
+If you would like to add to the existing data, visit [KeralaFloodRescue.com](http://kerelafloodrescue.com).
+Pull requests are welcome if you would like to improve this website.
---
+++ scripts/app.js
@@ -0,0 +1,160 @@
+var all_entries //to store json from google sheets
+
+
+$(document).ready(function() {
+    writeJSON()
+    .then(() => {
+        populateFilters();
+        renderBoxes();
+    })
+    $('select').on('change', function(){
+        selected_district = $('#District').find(':selected').text()
+        selected_type = $('#Type').find(':selected').text()
+        selected_status = $('#Status').find(':selected').text()
+        filteredData = all_entries.filter((x) => {
+            district_filter = selected_district == "All Districts" ? true : x.gsx$district.$t.trim() == selected_district
+            type_filter = selected_type == "All Types" ? true : x.gsx$typeofservice.$t.trim() == selected_type
+            status_filter = false;
+            status_text = x.gsx$status.$t.trim()
+            switch(selected_status){
+                case "All Status":
+                    status_filter = true;
+                    break;
+                case "Active":
+                    acceptable_status = ["Active", "Active.","ACTIVE","active","active.","ACTIVE."]
+                    status_filter =  acceptable_status.includes(status_text)?true : false
+                    break;
+                case "Inactive":
+                    acceptable_status = ["Inactive", "Inactive?", "Not active","NOT ACTIVE"]
+                    status_filter =  acceptable_status.includes(status_text)?true : false
+                    break;
+                case "Others":
+                    unacceptable_status = ["Active", "Active.","ACTIVE","active","active.","ACTIVE.",
+                    "Inactive", "Inactive?", "Not active","NOT ACTIVE"]
+                    if(unacceptable_status.includes(status_text)){
+                        status_filter = false
+                    } else {
+                        status_filter = true
+                    }
+                    break;
+            }
+            return district_filter&&status_filter&&type_filter
+        })
+        renderSelectedData(filteredData)
+    })
+
+})
+
+
+
+function getJSON(){
+    //returns formatted json from Rescue Ops google sheets
+    return new Promise((resolve, reject) => {
+    url = 'https://spreadsheets.google.com/feeds/list/1DtMZWjONp6Nh3hUmGXpq3r83OkTV_kKokYdDnTu6vWc/od6/public/values?alt=json'
+    fetch(url)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(formattedData){
+        entries = formattedData.feed.entry
+        resolve(entries);
+    })
+})
+}
+
+function getFilters(){
+    //get list of districts, types and status for filtering
+    //entries = JSON.parse(localStorage.getItem('allEntries'))
+    entries = all_entries
+        districts = {};
+        types = {};
+        condition = {};
+        for(i=0;i<entries.length;i++){
+            row = entries[i];
+            district = row.gsx$district.$t.trim()
+            type = row.gsx$typeofservice.$t.trim()
+            retrived_status = row.gsx$status.$t.trim()
+            condition[retrived_status] = 1
+            districts[district] = 1
+            types[type] = 1
+        }
+        data = {districts: Object.keys(districts),
+                types: Object.keys(types),
+                status: Object.keys(condition)}
+        return(data)
+}
+
+function writeJSON(){
+    //writes downloaded json into --localstorage-- variable
+    return new Promise((resolve, reject) => {
+    getJSON()
+    .then((data)=>{
+        //localStorage.setItem('allEntries', JSON.stringify(data));
+        all_entries = entries
+        resolve()
+    })
+})
+}
+
+function populateFilters(){
+    //populates filter dropdowns
+    data = getFilters()
+    for(i=0;i<data.districts.length;i++){
+        $('#District').append(new Option (data.districts[i], data.districts[i]))
+    }
+    for(i=0;i<data.types.length;i++){
+        $('#Type').append(new Option (data.types[i], data.types[i]))
+    }
+    /** 
+    for(i=0;i<data.status.length;i++){
+        $('#Status').append(new Option (data.status[i], data.status[i]))
+    }
+    */
+}
+
+function renderBoxes(){
+    var elements = $();
+    //entries = JSON.parse(localStorage.getItem('allEntries'))
+    entries = all_entries
+    renderSelectedData(entries)
+}
+
+
+function renderSelectedData(data){
+    $('#data_boxes').empty();
+    if(data.length == 0){
+        notification_element = `<div class="notification is-info">
+        No entries with selected filters found. Kindly try changing the filters.
+      </div>`
+      $('#data_boxes').append(notification_element)
+    } else{
+    var elements = $();
+    entries = data
+    for(i=0;i<entries.length;i++){
+        elem = `<div class="box">
+        <div class="columns">
+                <div class="column">
+                  <h1 class="title is-4"><span id="district">${entries[i].gsx$district.$t.trim()}</span></h1>
+                  <strong>Name: </strong><span id="name">${entries[i].gsx$name.$t.trim()}</span><br>
+                  <strong>Contact Number: </strong><span id="number">${entries[i].gsx$contactnumber.$t.trim()}</span><br>
+                  <strong>Type of Service: </strong><span id="number">${entries[i].gsx$typeofservice.$t.trim()}</span><br>
+                  <strong>Location: </strong> <span id="location">${entries[i].gsx$location.$t.trim()}t<span>
+                  <br>
+                  <strong>Status: </strong> <span id="status">${entries[i].gsx$status.$t.trim()}</span><br>
+                  <strong>Date: </strong> <span id="data">${entries[i].gsx$statusason.$t.trim()}</span><br>
+                  <strong>Volunteer in charge: </strong> <span id="volunteer">${entries[i]['gsx$volunteer-in-charge'].$t.trim()}</span>
+                </div>
+                <div class="column">
+                    <strong>Details: </strong><br>
+                    ${entries[i].gsx$details.$t.trim()}
+                </div>
+                <div class="column">
+                  Map
+                </div>
+              </div>
+</div>`
+        elements = elements.add(elem)
+    }
+    $('#data_boxes').append(elements)
+}
+}
